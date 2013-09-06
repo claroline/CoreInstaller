5ffabc88b23700c52afc25b0dc6869d4e88da85b
Stéphane Klein <stephaneklein221@gmail.com>
2013-09-06 21:57:02 +0200

    Formatted logger output

--- Installer.php
+++ Installer.php
@@ -195,7 +195,7 @@ class Installer extends LibraryInstaller
         $baseInstaller = $this->kernel->getContainer()->get('claroline.installation.manager');
         $io = $this->io;
         $baseInstaller->setLogger(function ($message) use ($io) {
-            $io->write($message);
+            $io->write("    {$message}");
         });
 
         return $baseInstaller;
